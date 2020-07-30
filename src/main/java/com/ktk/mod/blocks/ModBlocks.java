9c8f59c25deecc5266cd82747e1872b3a398f6e6
wenjunma004 <wenjunma004@gmail.com>
2020-07-29 20:24:32 -0700

    check in

---
+++ src/main/java/com/ktk/mod/blocks/ModBlocks.java
@@ -0,0 +1,8 @@
+package com.ktk.mod.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+    @ObjectHolder("mythrilmod:firstblock")
+    public static FirstBlock FIRSTBLOCK;
+}
